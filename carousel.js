5955c7aed94dc1675288435214908b2f3bc46f69
Yamamoto risako <go.to.orbit.utyu2@gmail.com>
2017-03-16 13:28:56 +0000

    １行しか収納されないけど保存

---
+++ carousel.js
@@ -0,0 +1,13 @@
+// JavaScript File
+
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel(
+        {
+            loop: true,
+            nav: false,
+            dots: true,
+            items: 1,
+            autoplay: true
+        }
+    );
+});
